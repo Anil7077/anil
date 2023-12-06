62254d9b146963f9e8ced943a7ce858c9b56200a
anil.mallick2002@gmail.com <anil.mallick2002@gmail.com>
2023-12-06 10:26:00 +0530

    fixed3

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,10 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <div>
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
-          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous" />
-
-        </div>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-          crossOrigin="anonymous"
-          async
-        ></script>
-
-        <link rel="stylesheet" href="/css/style.css"></link>
-      </Head>
+      
       <Component {...pageProps} />
     </>
   )
